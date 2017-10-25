15e2e827155510db30cbdae9e127739a65404abe
huangYFfromZERO <30410533+huangYFfromZERO@users.noreply.github.com>
2017-10-25 19:42:18 +0800

    Create 默认构造函数的实验.cpp

---
+++ 默认构造函数的实验.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+class a
+{
+	int c;
+};
+class b
+{
+	int c;
+	public:
+	b(int dd):c(dd){}
+	b(){}; //= default;
+};
+int main()
+{
+	a temp1;
+	b temp2;
+}
